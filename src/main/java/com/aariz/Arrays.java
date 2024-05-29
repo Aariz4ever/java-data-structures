fddad8afb161d827da5d71544cd5b3e64a64dfff
Aariz4ever <aariz4ever@gmail.com>
2024-05-29 14:13:06 +0300

    first commit

---
+++ src/main/java/com/aariz/Arrays.java
@@ -0,0 +1,13 @@
+package com.aariz;
+import java.util.Arrays;
+
+public class Arrays {
+    public static void main(String[] args){
+        String[] colors = new String[5];
+        colors[0] = "red";
+        colors[1] = "blue";
+        for (int i = 0; i < colors.length; i++){
+            System.out.println(colors[i]);
+        }
+    }
+}
